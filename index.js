b11a1577b21a9e6fe3a938c8c8da34d3dc424e08
james <jamesdeleon373@gmail.com>
2023-04-11 23:21:30 +0800

    Added initial frontend for the calculator

---
+++ index.js
@@ -0,0 +1,28 @@
+const calculatorContainer = document.getElementById('calc-container');
+
+
+
+
+function add (x,y) {
+
+}
+
+function subtract (x,y) {
+
+}
+
+function multiply (x,y) {
+
+}
+
+function divide (x,y) {
+
+}
+
+function modulo (x,y) {
+
+}
+
+function evaluate (num1, num2, operator) {
+    
+}
